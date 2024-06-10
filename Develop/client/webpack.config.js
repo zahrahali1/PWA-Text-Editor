3794ba0d62189be412724fb52ab267d7fd2d6405
zahrahali1 <zahrahali2000@hotmail.com>
2024-06-10 10:32:25 +0100

    updated webpack config.js

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -24,13 +24,53 @@ module.exports = () => {
         template: './index.html',
         title: 'Contact Cards',
       }),
+
+      // Injects our custom service worker
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+       // Creates a manifest .json file 
+       new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Jate Settings',
+        short_name: 'Jate',
+        description: 'A simple text editor',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+       })
       
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'], 
+            },
+          },
+        },        
       ],
     },
-  };
+  };      
 };
