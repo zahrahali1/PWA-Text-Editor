a3370d33c120cbe75a6035d75e5912d3f2b98159
zahrahali1 <zahrahali2000@hotmail.com>
2024-06-10 11:06:29 +0100

    database.js updated

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -1,3 +1,4 @@
+import { request } from 'express';
 import { openDB } from 'idb';
 
 const initdb = async () =>
@@ -7,15 +8,39 @@ const initdb = async () =>
         console.log('jate database already exists');
         return;
       }
+      // Creates a new object store for the data
       db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
       console.log('jate database created');
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  // Open the database
+  const db = await openDB('jate', 1);
+  // Access the object store
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  // Add the content to the store
+  await store.put(content);
+  // Complete the transaction
+  await tx.done;
+  console.log('Content added to database:', content);
+};
+
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const db = await openDB('jate', 1);
+  // Access the object store
+  const tx = db.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  // Retrieve all content from the store
+  const request = store.getAll();
+  // Get confirmation fo the request
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
 
 initdb();
