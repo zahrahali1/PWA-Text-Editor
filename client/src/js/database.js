a29237c1dc610d7a128c43f96859697f881921c3
zahrahali1 <zahrahali2000@hotmail.com>
2024-06-20 22:41:29 +0100

    Added put and get content to the database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,7 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-// Storing data from the database 
+// Storing data from the database  
 export const putDb = async (content) => {
   console.log('PUT to the database');
   const jateDb = await openDB('jate', 1);
